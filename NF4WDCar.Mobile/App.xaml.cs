2bd649b4247870431d7de833f1d29055bd0099d3
Artiom-Evs <artiom.evs@gmail.com>
2024-01-07 00:47:11 +0100

    Add .NET MAUI mobile app project
    This mobile app is a client for car control.

---
+++ NF4WDCar.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NF4WDCar.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ NF4WDCar.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NF4WDCar.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
